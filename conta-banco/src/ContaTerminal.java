a0e8042a51bed3e1fc2a5f045c85cca0d17a63b2
Adilsontad <adianjohot@gmail.com>
2024-06-02 20:00:39 -0300

    meu primeiro projeto está pronto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,9 +2,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    @SuppressWarnings("resource")
+    public static void main(String[] args){ 
+          Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu Nome:");
         String nomeCliente = scanner.next();
@@ -15,15 +16,20 @@ public class ContaTerminal {
         System.out.println("Digite o numero da sua conta:");
         int conta = scanner.nextInt();
 
-        System.out.println("Digite seu Nome:");
+        
+        System.out.println("Digite o seu saldo:");
         Double saldo = scanner.nextDouble();
-
+            
         //"Olá, [NomeCliente] , obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque"
 
-        System.out.println("Olá," + nomeCliente + "" + ", obrigado por criar uma conta em nosso banco,");
-        System.out.println("sua agência é:"  + agencia);
-        System.out.println("conta" + conta);
-        System.out.println("e seu saldo:" + saldo + "já está disponível para saque");
-        
-    }
-}
+        System.out.println("Olá, " + nomeCliente + "" + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é: "  + agencia);
+        System.out.println("conta: " + conta);
+        System.out.println("e seu saldo: " + saldo + " já está disponível para saque");
+    
+
+
+                }
+
+
+            }
