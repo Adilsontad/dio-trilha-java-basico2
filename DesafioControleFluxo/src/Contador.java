93a42ebf45d0c708b4c9e2fed19571f88f661688
Adilsontad <adianjohot@gmail.com>
2024-06-06 22:47:34 -0300

    primeira tentativa

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+  
+    public static void main(String[] args) throws Exception {
+Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
+ 
+    System.out.println("Digite o primeiro valor: ");
+    int valor1 = scanner.nextInt();
+    
+    System.out.println("Digite o segundo valor: ");
+    int valor2 = scanner.nextInt();
+
+    if 
+    (valor1 >= valor2);
+    
+    System.out.println("O primeiro valor deve ser menor que o segundo");
+    
+do
+      System.out.println(valor1++);
+
+            while (valor1 <= valor2);
+
+            scanner.close();
+}
+
+    }
+
