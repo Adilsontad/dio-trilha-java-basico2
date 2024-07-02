171d162abb1954c2465882bf6bd15971bab24a2d
Adilsontad <adianjohot@gmail.com>
2024-07-02 00:46:40 -0300

    treinando operações básicas SET

---
+++ meu-collections/carrinho-de-compras/src/set/OperacoesBasicas/src/conjuntoConvidado/Convidado.java
@@ -0,0 +1,52 @@
+package conjuntoConvidado;
+public class Convidado {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    private String nome;
+
+    private int codigoConvite;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado [nome=" + nome + ", codigoConvite=" + codigoConvite + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigoConvite;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Convidado other = (Convidado) obj;
+        if (codigoConvite != other.codigoConvite)
+            return false;
+        return true;
+    }
+    
+
+}
